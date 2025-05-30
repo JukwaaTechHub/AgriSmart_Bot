c2da79455c92b28b1c0dd8eed9efff1efcc298e2
TariqNew <marandumashaka78@gmail.com>
2025-05-30 20:25:45 +0300

    Delete languageDetector.js

--- utils/languageDetector.js
+++
@@ -1,8 +0,0 @@
-function detect(text) {
-  const swahiliKeywords = ["dudu", "majani", "nyanya", "mahindi", "kudhibiti"];
-  const lowered = text.toLowerCase();
-  const swCount = swahiliKeywords.reduce((acc, kw) => (lowered.includes(kw) ? acc + 1 : acc), 0);
-  return swCount > 0 ? "sw" : "en";
-}
-
-module.exports = { detect };
